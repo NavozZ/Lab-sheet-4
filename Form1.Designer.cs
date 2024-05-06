b7ab2b766282bfdcfa491a1c865268f9936629fe
NavozZ <135834866+NavozZ@users.noreply.github.com>
2024-05-06 10:21:27 +0530

    Question 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Question1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SumLabel = new System.Windows.Forms.Label();
+            this.Num1 = new System.Windows.Forms.TextBox();
+            this.Num2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number 01";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number 02";
+            // 
+            // SumLabel
+            // 
+            this.SumLabel.AutoSize = true;
+            this.SumLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SumLabel.Location = new System.Drawing.Point(77, 237);
+            this.SumLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.SumLabel.Name = "SumLabel";
+            this.SumLabel.Size = new System.Drawing.Size(82, 22);
+            this.SumLabel.TabIndex = 2;
+            this.SumLabel.Text = "Addition";
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(222, 91);
+            this.Num1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(66, 20);
+            this.Num1.TabIndex = 3;
+            this.Num1.TextChanged += new System.EventHandler(this.Num1_TextChanged);
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(222, 165);
+            this.Num2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(66, 20);
+            this.Num2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(388, 301);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.SumLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label SumLabel;
+        private System.Windows.Forms.TextBox Num1;
+        private System.Windows.Forms.TextBox Num2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
